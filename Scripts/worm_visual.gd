691ad4b965fd4270ce83970c8fca8c3e41abb3ea
SnowingLizard56 <77141586+SnowingLizard56@users.noreply.github.com>
2025-10-25 10:45:36 +1300

    Wormier Worm

--- Scripts/worm_visual.gd
+++ Scripts/worm_visual.gd
@@ -31,9 +31,17 @@ const OCTAVE_CENTRES: PackedFloat32Array = [
 const OCTAVE_COUNT: int = 11
 const MIN_DB := 60.0
 # Drawing Parameters
-const POINT_COUNT := 80
-const LENGTH := 160.0
-const MAX_MAGNITUDE := 10.0
+const COLOUR_ALTERNATE_COUNT := 8
+const COLOUR_ONE := Color(0.776, 0.6, 0.639, 1.0)
+const COLOUR_TWO := Color(0.576, 0.353, 0.4, 1.0)
+const COLOUR_BORDER := Color(0.4, 0.239, 0.278, 1.0)
+
+const POINT_COUNT := 160
+const WORM_LENGTH := 160.0
+const MAX_MAGNITUDE := 20.0
+
+const WORM_RADIUS := 6.0
+const BORDER_RADIUS := 3.0
 
 
 var current_magnitudes: PackedFloat32Array = []
@@ -44,13 +52,13 @@ func _ready() -> void:
 	if get_tree().current_scene == self:
 		$TestCamera.enabled = true
 		$TestAudio.playing = true
-	spectrum = AudioServer.get_bus_effect_instance(0,0)
+	spectrum = AudioServer.get_bus_effect_instance(0,1)
 	current_magnitudes.resize(OCTAVE_COUNT)
 	current_magnitudes.fill(0.0)
 
 
 func get_current_magnitudes():
-	var magnitudes : PackedFloat32Array = []
+	var magnitudes: PackedFloat32Array = []
 	
 	for i in OCTAVE_COUNT:
 		# x component is magnitude of sound in left ear, y is magnitude of sound in right ear
@@ -71,7 +79,9 @@ func get_current_magnitudes():
 func sample(magnitudes: PackedFloat32Array, t: float):
 	var total: float = 0.0
 	for i in OCTAVE_COUNT:
-		total += magnitudes[i] * sin(TAU * t / OCTAVE_CENTRES[i])
+		if is_zero_approx(magnitudes[i]):
+			continue
+		total += magnitudes[i] * sin(TAU / OCTAVE_CENTRES[i] * t)
 	return total
 
 
@@ -91,12 +101,23 @@ func _draw() -> void:
 	pts.resize(POINT_COUNT)
 	for i in POINT_COUNT:
 		pts[i] = Vector2(
-			-LENGTH * i / POINT_COUNT,
-			MAX_MAGNITUDE * sample(current_magnitudes, LENGTH * i / POINT_COUNT)
+			-WORM_LENGTH * i / POINT_COUNT,
+			MAX_MAGNITUDE * sample(current_magnitudes, WORM_LENGTH * i / POINT_COUNT)
 		)
-	# Just a basic polyline for now
-	draw_polyline(
-		pts,
-		Color.WHITE,
-		5.0
-	)
+	# Border
+	for i in pts:
+		draw_circle(i, WORM_RADIUS + BORDER_RADIUS, COLOUR_BORDER)
+	
+	var remaining := 1
+	var current_colour := COLOUR_ONE
+	# Go backwards for the circles to stack properly
+	for i in range(POINT_COUNT - 1, -1, -1):
+		remaining -= 1
+		if remaining == 0:
+			remaining = COLOUR_ALTERNATE_COUNT
+			if current_colour == COLOUR_ONE:
+				current_colour = COLOUR_TWO
+			else:
+				current_colour = COLOUR_ONE
+		
+		draw_circle(pts[i], WORM_RADIUS, current_colour)
