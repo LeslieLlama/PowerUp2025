b2372041431f5f13f6c0bed969dc9b7ee8da0f06
SnowingLizard56 <77141586+SnowingLizard56@users.noreply.github.com>
2025-10-28 08:05:54 +1300

    Fixes no AudioEffect in web export
    also fixes a circular dependency issue I was having that stopped me from opening. anything

--- Scripts/level_button.gd
+++ Scripts/level_button.gd
@@ -1,9 +1,9 @@
 extends Button
 
-@export var scene_to_load : PackedScene
+@export var path_to_scene_to_load : String
 
 func _on_pressed() -> void:
 	call_deferred("next_level")
 	
 func next_level():
-	get_tree().change_scene_to_packed(scene_to_load)
+	get_tree().change_scene_to_file(path_to_scene_to_load)
